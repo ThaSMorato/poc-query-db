c04eb44bf7441bf8a6c2e7d7268d71f52a3912e2
Thales Morato <tha.s.morato@gmail.com>
2021-11-25 09:32:32 -0300

    100%

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
@@ -14,17 +14,37 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.find({
+      where: {
+        id: user_id,
+      },
+      relations: ["games"],
+    });
+
+    return user[0];
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const first_name_correct = first_name
+      .toLowerCase()
+      .split("")
+      .map((x, i) => (i == 0 ? x.toUpperCase() : x))
+      .join("");
+    const last_name_correct = last_name
+      .toLowerCase()
+      .split("")
+      .map((x, i) => (i == 0 ? x.toUpperCase() : x))
+      .join("");
+    return this.repository.query(
+      "SELECT * FROM users WHERE first_name = $1 AND last_name = $2",
+      [first_name_correct, last_name_correct]
+    ); // Complete usando raw query
   }
 }
